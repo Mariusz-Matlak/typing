447c44e94733520375731f795dd5240a593c5e6d
Mariusz Matlak <1mariusz.matlak@gmail.com>
2019-04-17 11:19:42 +0200

    add logic

---
+++ main.js
@@ -0,0 +1,24 @@
+const spanText = document.querySelector('.text');
+const spanCursor = document.querySelector('.span');
+const text = ['tekst1', 'tekst2', 'tekst3'];
+
+let activeLetter = -20;
+let activeText = 0;
+
+const addLetter = () => {
+    if (activeLetter >= 0) {
+        spanText.textContent += text[activeText][activeLetter]
+    }
+    activeLetter++;
+    if (activeLetter === text[activeText].length) {
+        activeText++;
+        if (activeText === text.length) return;
+        return setTimeout(() => {
+            activeLetter = -20;
+            spanText.textContent = '';
+            addLetter();
+        }, 2000)
+    }
+    setTimeout(addLetter, 100);
+}
+addLetter();
